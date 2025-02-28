3d34b7ebeb3a0863bdce87c94b7c685aac120054
ivjoli <ivjoli@ucsc.edu>
2025-02-28 14:11:47 -0800

    getting rid of print statement

--- loading_data.py
+++ loading_data.py
@@ -1,8 +1,7 @@
 def loading_datasets(dataset):
     # open file
     df = xr.open_dataset(dataset)
-    print(df) # read the datafile
-
+    
     # store chlor_a dataset in variable first
     chlor_a = df.chlor_a #(can call chlor_a instead of df.chlor_a)
     return chlor_a
