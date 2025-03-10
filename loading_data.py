a7b12b4f1a9fa88c71c4b7f7221c410da6033c52
ivjoli <ivjoli@ucsc.edu>
2025-03-09 18:34:00 -0700

    adding coastlines to load

--- loading_data.py
+++ loading_data.py
@@ -5,3 +5,10 @@ def loading_datasets(dataset):
     # store chlor_a dataset in variable first
     chlor_a = df.chlor_a #(can call chlor_a instead of df.chlor_a)
     return chlor_a
+
+def loading_coastline():
+    #Load coastline data
+    shp_path = shpreader.natural_earth(resolution="110m", category="physical", name="coastline")
+    coastline_shapes = list(shpreader.Reader(shp_path).geometries())
+    coastline = unary_union(MultiLineString(coastline_shapes))  # Combine into a single MultiLineString
+    return coastline
